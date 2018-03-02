7414482708fc45092fa5b3beb9e6c1bc927aca3d
Agustín <agustin.golmar@gmail.com>
2018-03-02 17:03:36 -0300

    Master branch.

---
+++ README.md
@@ -0,0 +1,16 @@
+![...](res/image/readme-header.png)
+
+# System Simulation: Cell Index Method
+
+...
+
+## Deployment
+
+...
+
+## Designers
+
+El proyecto fue construido, es diseñado y mantenido por:
+
+* @lobo
+* @agustin-golmar
---
+++ src/main/java/ar/edu/itba/ss/Main.java
@@ -0,0 +1,14 @@
+
+	package ar.edu.itba.ss;
+
+		/**
+		* <p>Puto de entrada principal de la simulación.</p>
+		*/
+
+	public final class Main {
+
+		public static void main(final String [] arguments) {
+
+			System.out.println("(2018) Cell Index Method.");
+		}
+	}
